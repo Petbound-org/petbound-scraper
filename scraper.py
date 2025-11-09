a7ca8668b8f0987a5c3024ceae20b60110796523
Mahasvin Shanmugapriya Manikandan <mahasvin.manikandan@gmail.com>
2025-11-09 00:15:27 -0800

    Supabase connections works now Requirements.txt has also been update I'm gonna call it a night if someone is actually reading this then hello

--- scraper.py
+++ scraper.py
@@ -1,39 +1,46 @@
 # scraper.py
 
 # imports
+import os
 import requests
-from bs4 import BeautifulSoup
 import time
+import csv
 import re # regular expressions
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import csv
+from supabase import create_client, Client
+from dotenv import load_dotenv
 
-# Params
-states = ['AZ', 'CA', 'FL', 'GA', 'NC', 'OH', 'OK', 'TX']
-BASE = "https://www.dogsindanger.com/searchReturn_desktop.jsp?BREED=&t=90&startId={start_index}&zip=&radius=100.0&state={state}&Transport=0"
+# Database Connection
+load_dotenv()
+supabase: Client = create_client(
+    os.environ['SUPABASE_URL'],
+    os.environ['SUPABASE_KEY']
+)
 
 def get_dog_ids():
     """
     Getting all dog_ids on the dogsindanger website.
+    Scraper en
 
     Each dog's page can be accessed by:
     dogsindanger.com/dog/<dog_id>
     """
+    BASE = "https://www.dogsindanger.com/searchReturn_desktop.jsp?BREED=&t=90&startId={start_index}&zip=&radius=100.0&state={state}&Transport=0"
+    states = ['AZ', 'CA', 'FL', 'GA', 'NC', 'OH', 'OK', 'TX']
     dogs_ids = []
 
     # Repeating for each state
     for state in states:
-        # Accounting for index and end page
         start_index = 0
-        complete = False
         
         # Looping through start_indicies
         while True:
             # Fetching a response
             url = BASE.format(start_index=start_index, state=state)
             response = requests.get(url)
-            time.sleep(0.5) # for the sake of the server (DO NOT REMOVE, BAD THINGS MAY HAPPEN)
+            # time.sleep(0.5) # *** UNCOMMENT AS A POSSIBLE FIX FOR UNEXPECTED ERRORS ***
             
             # Ensure Page Exists (CRITICAL ERROR IF FAILS)
             try: 
@@ -47,14 +54,14 @@ def get_dog_ids():
 
             # END condition, start_index is too high, scraping for state complete
             if "There are no dogs matching your search criteria." in response.text:
-                break
+                break # the break here is used responsibly (I hope)
             
             # HTML parsing
             soup = BeautifulSoup(response.text, 'html.parser')
             dog_divs = soup.find_all('div', attrs={'style': 'border-radius:5px;border:2px solid #999;background-color:white;box-shadow:0px 0px 10px #888;position:relative;margin-bottom:9px;'})
             for div in dog_divs:
                 href_val = div.find_all('a')[0]['href']
-                id = re.search(r'/dog/(\d+)-', href_val).group(1) # extracting id using regular expressions
+                id = re.search(r'/dog/(\d+)-', href_val).group(1) # may have AttributeError if link is unexpected, theoretically should not happen
                 dogs_ids.append(id)
 
             # Increment
@@ -64,11 +71,47 @@ def get_dog_ids():
 
     return dogs_ids
 
-if __name__ == '__main__':
+def test_get_dog_ids():
     start = time.time()
     ids = get_dog_ids()
     end = time.time()
 
-    print(f"\nScraped {len(ids)} dogs.")
-    print(f"All ids are unique: {len(set(ids)) == len(ids)}.")
-    print(f"Total duration: {(end - start):.2f}s.\n")
+    print(f"\nScraped {len(ids)} dogs")
+    print(f"All ids are unique: {len(set(ids)) == len(ids)}")
+    print(f"Total duration: {(end - start):.2f}s")
+    print(f"Page scraping duration (avg, upper estim): {20 * ((end - start) / len(ids)):.3f}\n") # keep >0.5s, use time.sleep() if necessary
+
+def scrape_dog(id):
+    """
+    Scrapes the data of a dog given its ID.
+    
+    Each dog's page can be accessed by:
+    dogsindanger.com/dog/<ID>
+    """
+    url = f"dogsindanger.com/dog/{id}"
+    response = requests.get(url)
+
+def scrape():
+    """
+    Master function that needs to be called in 
+    """
+    pass
+
+if __name__ == '__main__':
+    test_pet = (
+        supabase.table('pets')
+        .select('*')
+        .eq('id', 1)
+        .execute()
+        .data
+    )
+
+    test_shelter = (
+        supabase.table('shelters')
+        .select('*')
+        .eq('id', 1)
+        .execute()
+        .data
+    )
+
+    print(test_shelter)
