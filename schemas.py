be6cbcfda26d6cc89f33cc18a697e97d91575727
Mahasvin Shanmugapriya Manikandan <mahasvin.manikandan@gmail.com>
2026-09-02 16:57:49 -0700

    new schemas to match db'

---
+++ schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field
+
+class Shelter(BaseModel):
+    name: str = ""
+    address: str = ""
+    city: str = ""
+    state: str = ""
+    phone_number: str = ""
+    email: str = ""
+
+class Dog(BaseModel):
+    name: str = ""
+    breed: str = ""
+    age: str = ""
+    gender: str = ""
+    size: str = ""
+    description: str = ""
+    euthanasia_date: str = ""
+    euthanasia_reason: str = ""
+    shelter_given_id: str = ""
+    image_urls: list[str] = Field(default_factory=list)
